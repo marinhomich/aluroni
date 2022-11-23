bd25339d7f5e0470826944b3b3e5b19e2b3d6092
Michel Marinho <michel.marinho04@outlook.com>
2022-11-23 11:10:39 -0400

    Update: Import SVG

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Cardapio from "./pages/cardapio";
 
 
 const root = ReactDOM.createRoot(
@@ -8,6 +9,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <div>Aluroni</div>
+    <Cardapio/>
   </React.StrictMode>
 );
