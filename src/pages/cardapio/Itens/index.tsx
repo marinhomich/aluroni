8e90cdd50f09e8dd4ff256780a98879e7708ec7d
Michel Marinho <michel.marinho04@outlook.com>
2022-11-23 16:23:19 -0400

    Feat: Items

---
+++ src/pages/cardapio/Itens/index.tsx
@@ -0,0 +1,14 @@
+import itens from './itens.json'
+import Item from './Item'
+import styles from './Itens.module.scss'
+
+export default function Itens(){
+    return (
+        <div className={styles.itens}>
+            {itens.map((item) => (
+                <Item key={item.id}  />
+            ))}
+
+        </div>
+    )
+}
