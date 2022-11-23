7726f72637c7a40ba9b4b87224b7967ccdfbf4da
Michel Marinho <michel.marinho04@outlook.com>
2022-11-23 11:32:47 -0400

    Feat: Variaveis scss

--- src/pages/cardapio/index.tsx
+++ src/pages/cardapio/index.tsx
@@ -7,6 +7,11 @@ export default function Cardapio() {
             <nav className={styles.menu}>
                 <Logo/>
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    kasmdkamsdkm
+                </div>
+            </header>
         </main>
     )
 }
