3e5121378bc6d7d4ac4beeac8242f06c22ee8caf
Michel Marinho <michel.marinho04@outlook.com>
2022-11-23 16:59:09 -0400

    Fix: Food Image

--- src/pages/cardapio/Itens/Item/index.tsx
+++ src/pages/cardapio/Itens/Item/index.tsx
@@ -1,29 +1,46 @@
-import logo from 'assets/logo.svg'
 import styles from './Item.module.scss'
+import classNames from "classnames";
 
-export default function Item() {
+interface Props {
+    title: string,
+    description: string
+    photo: string,
+    size: number,
+    serving: number,
+    price: number,
+    id: number,
+    category: {
+        id: number,
+        label: string
+    }
+}
+
+export default function Item({title, photo, description, category, size, serving, price }: Props) {
     return (
         <div className={styles.item}>
             <div className={styles.item__imagem}>
-                <img src={logo} alt='Imagem'/>
+                <img src={photo} alt={title} />
             </div>
             <div className={styles.item__descricao}>
                 <div className={styles.item__titulo}>
-                    <h2>Macarrão</h2>
-                    <p>Descrição do Macarrão</p>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
                 </div>
                 <div className={styles.item__tags}>
-                    <div className={styles.item__tipo}>
-                        Massa
+                    <div className={classNames({
+                        [styles.item__tipo]: true,
+                        [styles[`item__tipo__${category.label.toLowerCase()}`]]: true,
+                    })}>
+                        {category.label}
                     </div>
                     <div className={styles.item__porcao}>
-                        400g
+                        {size}
                     </div>
                     <div className={styles.item__qtdpessoas}>
-                        Serve 2 Pessoas
+                        Serve {serving} Pessoa{serving === 1 ? "" : "s"}
                     </div>
                     <div className={styles.item__valor}>
-                        R$50,00
+                        R${price.toFixed(2)}
                     </div>
                 </div>
             </div>
--- src/pages/cardapio/Itens/index.tsx
+++ src/pages/cardapio/Itens/index.tsx
@@ -6,7 +6,18 @@ export default function Itens(){
     return (
         <div className={styles.itens}>
             {itens.map((item) => (
-                <Item key={item.id}  />
+                <Item
+                    key={item.id}
+                    title={item.title}
+                    description={item.description}
+                    photo={item.photo}
+                    size={item.size}
+                    serving={item.serving}
+                    price={item.price}
+                    category={item.category}
+                    id={item.id}
+
+                />
             ))}
 
         </div>
